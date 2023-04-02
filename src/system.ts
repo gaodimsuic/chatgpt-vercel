747efc1bc86d343ad525b6d52e9b774e12068902
gaodimsuic <110146210+gaodimsuic@users.noreply.github.com>
2023-04-02 17:07:08 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,8 +11,8 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `Powered by OpenAI
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
-- 对话生成图片方式为：电脑复制到剪贴板，手机直接下载。`
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
+- 对话生成图片方式为：电脑复制到剪贴板，手机直接下载。
 - 对话生成 Markdown，复制到剪贴板。`
 
 export type Setting = typeof defaultSetting
